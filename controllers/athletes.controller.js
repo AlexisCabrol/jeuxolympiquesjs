dcc1b693b2ce0c1b898b52747e9292b7a19c0d69
Alexis Cabrol <alexiscabrololivaux@gmail.com>
2020-11-05 17:53:53 +0100

    Création d'un athlète et liste d'un athlète

--- controllers/athletes.controller.js
+++ controllers/athletes.controller.js
@@ -1,5 +1,34 @@
+const AthletesService = require('../services/athletes.service');
+const Athlete = require('../models/athlete');
+
 class AthletesController {
 
+    /**
+     * Constructeur.
+     */
+    constructor() {
+        this.athletesService = new AthletesService();
+    }
+
+    /**
+     * Création d'un athlète.
+     * @param {req} req la requête.
+     * @param {res} res le résultat.
+     */
+    async creationAthlete(req, res) {
+        await this.athletesService.creationAthlete(req);
+        res.redirect('/api/sports');
+    }
+
+    /**
+     * Récupération d'une liste des athlètes.
+     * @param {req} req la requête.
+     * @param {res} res la résultat.
+     */
+    async getListeDesAthletes(req, res) {
+        const athletes = await this.athletesService.getListeDesAthletes();
+        res.render('athletes', { athletes });
+    }
 }
 
 module.exports = AthletesController;
--- services/athletes.service.js
+++ services/athletes.service.js
@@ -1,5 +1,30 @@
+const Athletes = require('../models/athlete');
+
 class AthletesService {
 
+    /**
+     * Création d'un athlète.
+     * @param {req} paramètre de la requête. 
+     */
+    async creationAthlete(req) {
+        const paramAthelete = req.body;
+        const athlete = new Athlete({
+            firstName: paramAthelete.firstName,
+            lastName: paramAthelete.lastName,
+            gender: paramAthelete.gender,
+            country: paramAthelete.country,
+        });
+        await athlete.save();
+    }
+
+    /**
+     * Récupération d'une liste des athlètes.
+     * @return la liste des athlètes en base.
+     */
+    async getListeDesAthletes() {
+        const athletes = await Athletes.find({});
+        return athletes;
+    }
 }
 
 module.exports = AthletesService;
