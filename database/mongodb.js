a21dfdf5fb02f73d876f0f61998e290f838bf237
Luc Hyvernat <luccho7.lh@gmail.com>
2020-11-05 16:22:31 +0100

    Ajout du lien BDD

--- database/mongodb.js
+++ database/mongodb.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect('');
+        await mongoose.connect('mongodb+srv://Admin:2uOvaEVmGW0BCU7q@firstcluster.jrlpb.mongodb.net/jeuxOlympiquesJS?retryWrites=true&w=majority', { useNewUrlParser: true });
     } catch(err) {
         console.error('Impossible de se log à la BDD.', err);
         throw err;
