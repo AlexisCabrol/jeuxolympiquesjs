dac06e1b9cf2c590ca729c4227a4cf3c43e06517
Alexis Cabrol <alexiscabrololivaux@gmail.com>
2020-11-12 15:30:09 +0100

    Externalisation des données de configuration dans un fichier .env

--- database/mongodb.js
+++ database/mongodb.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb+srv://test:test@firstcluster.jrlpb.mongodb.net/jeuxOlympiquesJS?retryWrites=true&w=majority', { useNewUrlParser: true });
+        await mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
     } catch(err) {
         console.error('Impossible de se log à la BDD.', err);
         throw err;
